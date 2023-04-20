644966da50444f02794435f34bf764d35dac19ec
Joshua Okorie <joshuaokorie008@gmail.com>
2023-04-20 10:29:46 +0100

    complete forgot password page functionality

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import OAuth from '../Components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 
 export default function ForgotPassword() {
@@ -10,6 +12,18 @@ export default function ForgotPassword() {
   function onchange(e) {
    setEmail(e.target.value)
   }
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email was sent")
+    } catch (error) {
+      toast.error("Could not reset password")
+    }
+    
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold '>Forgot Password</h1>
@@ -18,7 +32,7 @@ export default function ForgotPassword() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" className='w-full rounded-2xl' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onchange} placeholder='Email Address' />
           
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
